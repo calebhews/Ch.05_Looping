ac10861f2e38338338200cccb467c1aee5171b39
calebhews <calebhews@gmail.com>
2019-11-08 13:56:02 -0600

    Chapter 3

--- 5.1_Coin_Toss.py
+++ 5.1_Coin_Toss.py
@@ -1,13 +1,25 @@
-'''
-COIN TOSS PROGRAM
------------------
-1.) Create a program that will print a random 0 or 1.
-2.) Instead of 0 or 1, print heads or tails. Do this using if statements. Don't select from a list.
-3.) Add a loop so that the program does this 50 times.
-4.) Create a running total for the number of heads and the number of tails and print the total at the end.
-'''
+#
+# COIN TOSS PROGRAM
+# -----------------
+# 1.) Create a program that will print a random 0 or 1.
+# 2.) Instead of 0 or 1, print heads or tails. Do this using if statements. Don't select from a list.
+# 3.) Add a loop so that the program does this 50 times.
+# 4.) Create a running total for the number of heads and the number of tails and print the total at the end.
+#
 
-
+import random
+heads=0
+tails=0
+for i in range(50):
+    N=random.randrange(2)
+    if N==0:
+        print("Heads")
+        heads+=1
+    else:
+        print("Tails")
+        tails+=1
+print("Total Heads: ",heads)
+print("Total Tails: ",tails)
 
 
 
