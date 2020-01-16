00e8a70e2338ecffc3b020d21a973864ddaef23b
calebhews <calebhews@gmail.com>
2020-01-16 09:39:58 -0600

    Chapter 5 Jedi Training

--- 5.0_Jedi_Training.py
+++ 5.0_Jedi_Training.py
@@ -10,24 +10,24 @@ for i in range(3):
     num = int(input("Enter a number: "))
     total = total + num
 print("The total is:", total)
-  
-
-
 '''
   2. Write a Python program that will use a FOR loop to print the even
      numbers from 2 to 100, inclusive.
 '''
-
-
-
-
-
+for i in range(2,101,2):
+    print(i)
 '''
   3. Write a program that will use a WHILE loop to count from
      10 down to, and including, 0. Then print the words Blast off! Remember, use
      a WHILE loop, don't use a FOR loop.
 '''
-
+i = 10
+while i>0:
+    print(i)
+    i-=1
+    if i==0:
+        print("Blast off!")
+        break
 
 
 
@@ -37,9 +37,9 @@ print("The total is:", total)
   4. Write a program that prints a random integer from 1 to 10 (inclusive).
 '''
 
-
-
-
+import random
+num = random.randint(1,10)
+print(num)
 
 
 '''
@@ -52,3 +52,20 @@ print("The total is:", total)
      if statements.
       
 '''
+total = 0
+negative = 0
+zero = 0
+positive = 0
+for i in range(7):
+    i = int(input("Enter a number;"))
+    total += i
+    if i > 0:
+        positive +=1
+    elif i == 0:
+        zero += 1
+    else:
+        negative += 1
+print("The total sum was",total)
+print("The amount of negative numbers was",negative)
+print("The amount of times zero was put in was",zero)
+print("The amount of positive numbers was",positive)
--- 5.2_Roshambo.py
+++ 5.2_Roshambo.py
@@ -11,6 +11,56 @@ When the user quits print a win/loss record
 
 '''
 
+done = False
+wins = 0
+losses = 0
+while not done:
+
+    #variables
+
+    import random
+    num=random.randint(1,3)
+    x = int(input("1 = rock  2 = paper  3 = scissors"))
+    if num==1:
+        print("rock")
+        if x == 1:
+            print("Tie game")
+        elif x==2:
+            print("You win")
+            wins +=1
+        elif x==3:
+            print("You lose")
+            losses +=1
+        else:
+            done = True
+    elif num==2:
+        print("paper")
+        if x==1:
+            print("You win")
+            wins +=1
+        elif x==2:
+            print("Tie game")
+        elif x==3:
+            print("You lose")
+            losses +=1
+        else:
+            done = True
+    elif num==3:
+        print("scissors")
+        if x==1:
+            print("You lose")
+            losses +=1
+        elif x==2:
+            print("You win")
+            wins +=1
+        elif x==3:
+            print("Tie game")
+        else:
+            done = True
+    print("Wins:",wins)
+    print("Losses:",losses)
+
+
 
 
 
